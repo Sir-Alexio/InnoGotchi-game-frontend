719569f38c4ef97d0bb5a974c3d1aea16aa3fd09
Alexey <lmokhareva@gmail.com>
2023-01-14 17:14:06 +0300

    Add controller for all innogotchi page view

---
+++ Controllers/AllInnogotchiListController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InnoGotchi_backend.Controllers;
+using InnoGotchi_backend.DataContext;
+using InnoGotchi_backend.Models;
+
+namespace InnoGotchi_frontend.Controllers
+{
+    public class AllInnogotchiListController : Controller
+    {
+        PetController petController = new PetController(new ApplicationContext());
+        public IActionResult Index()
+        {
+            return View(petController.GetPets());
+        }
+    }
+}
