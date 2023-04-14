d48e676de2630ca4468b0eaf114b2f5904eb2043
Alexey <lmokhareva@gmail.com>
2023-04-14 18:04:23 +0300

    Add petValidation

--- Controllers/WebPetController.cs
+++ Controllers/WebPetController.cs
@@ -1,21 +1,31 @@
-﻿using InnoGotchi_backend.Models.DTOs;
+﻿using InnoGotchi_backend.Models;
+using InnoGotchi_backend.Models.Dto;
+using InnoGotchi_backend.Models.DTOs;
 using InnoGotchi_frontend.Models;
+using InnoGotchi_frontend.Models.Validators;
 using InnoGotchi_frontend.Services;
 using InnoGotchi_frontend.Services.Abstract;
+using InnoGotchi_frontend.Validation;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Text.Json;
 
 namespace InnoGotchi_frontend.Controllers
 {
     [Route("pet")]
     public class WebPetController : Controller
     {
-        private readonly IImageService _imageService;
-        
+        private readonly HttpClient _httpClient;
+        private static PetDto _pet;
 
-        public WebPetController(IImageService imageService)
+        public WebPetController(IHttpClientFactory httpClient)
         {
-            _imageService = imageService;
+            _httpClient = httpClient.CreateClient("Client");
+
+            if(_pet == null)
+                _pet = new PetDto(); 
         }
+
         [Route("constractor")]
         public IActionResult PetConstrator()
         {
@@ -23,19 +33,52 @@ namespace InnoGotchi_frontend.Controllers
         }
 
         [Route("pet-overview")]
-        public IActionResult GetPet(PetDto pet)
+        public IActionResult GetPetOverview(PetDto pet)
         {
             return View("PetOverview",pet);
         }
 
         public IActionResult CheckRadio(IFormCollection form)
         {
-            PetDto pet = new PetDto();
-            pet.Body = form["body"].ToString();
-            pet.Nose = form["nose"].ToString();
-            pet.Eyes = form["eye"].ToString();
-            pet.Mouth = form["mouth"].ToString();
-            return RedirectToAction("pet-overview","pet",pet);
+            _pet.Body = form["body"].ToString();
+            _pet.Nose = form["nose"].ToString();
+            _pet.Eyes = form["eye"].ToString();
+            _pet.Mouth = form["mouth"].ToString();
+
+            return RedirectToAction("pet-overview","pet",_pet);
+        }
+
+        [Route("new-pet")]
+        public async Task<IActionResult> OnPost(PetDto dto)
+        {
+            PetValidator validator = new PetValidator();
+
+            if (!validator.Validate(dto).IsValid)
+            {
+                return View("PetOverview", _pet);
+            }
+
+            _pet.PetName = dto.PetName;
+            
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Request.Cookies["token"]);
+
+            dto.HungerLevel = InnoGotchi_backend.Models.Enums.HungerLevel.Full;
+            dto.ThirstyLevel = InnoGotchi_backend.Models.Enums.ThirstyLevel.Full;
+
+            JsonContent content = JsonContent.Create(_pet);
+
+            HttpResponseMessage response = await _httpClient.PostAsync($"api/pet/new-pet", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                CustomExeption? errorMessage = JsonSerializer.Deserialize<CustomExeption>(response.Content.ReadAsStringAsync().Result);
+
+                ViewBag.Message = errorMessage.Message;
+
+                return View("PetOverview", _pet);
+            }
+
+            return View("FarmInfo", "farm");
         }
     }
 }
---
+++ Validation/PetValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using InnoGotchi_backend.Models.DTOs;
+
+namespace InnoGotchi_frontend.Validation
+{
+    public class PetValidator:AbstractValidator<PetDto>
+    {
+        public PetValidator()
+        {
+            RuleFor(x=>x.PetName).NotEmpty();
+        }
+    }
+}
