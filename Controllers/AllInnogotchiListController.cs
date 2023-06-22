8b4cae9cc1df4db740fd7068f342d43d2c5cd900
Alexey <lmokhareva@gmail.com>
2023-06-22 15:57:46 +0300

    Add all innogotchi list

--- Controllers/AllInnogotchiListController.cs
+++ Controllers/AllInnogotchiListController.cs
@@ -10,14 +10,16 @@ using System.Net.Http.Headers;
 using System.Net.Http;
 using InnoGotchi_frontend.Services;
 using InnoGotchi_frontend.Services.Abstract;
+using InnoGotchi_backend.Models.DTOs;
 
 namespace InnoGotchi_frontend.Controllers
 {
-    [Route("innogotches")]
+    [Route("innogotchi")]
     public class AllInnogotchiListController : Controller
     {
         private readonly HttpClient _client;
         private readonly ITokenService _tokenService;
+        private static List<PetDto> _pets;
 
         public AllInnogotchiListController(IHttpClientFactory clientFactory, ITokenService tokenService)
         {
@@ -25,20 +27,52 @@ namespace InnoGotchi_frontend.Controllers
             _tokenService = tokenService;
         }
 
-        public async Task<ActionResult> Index()
+        [Route("innogotches")]
+        public async Task<ActionResult> GetALlInnogotches()
         {
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Request.Cookies["token"]);
 
-            HttpResponseMessage response = await _client.GetAsync("api/pet");
+            HttpResponseMessage response = await _client.GetAsync("api/pet/pets");
 
             if (!response.IsSuccessStatusCode)
             {
                 return BadRequest("UnAuthorized");
             }
-            return RedirectToAction("Index", "account");
-            
+            List<PetDto>? pets = JsonSerializer.Deserialize<List<PetDto>>(response.Content.ReadAsStringAsync().Result);
+
+            pets = pets.OrderBy(x => x.HappyDaysCount).Reverse().ToList();
+
+            _pets = pets;
+
+            return View("InnogotchiList", pets);
         }
 
-        
+        [Route("sorted-innogotches")]
+        public async Task<ActionResult> SortedInnogotches(string selectedItem)
+        {
+            List<PetDto> pets = new List<PetDto>();
+            switch (selectedItem)
+            {
+                case "Happy days":
+                    pets = _pets.OrderBy(x => x.HappyDaysCount).Reverse().ToList();
+                    break;
+                case "Age":
+                    pets = _pets.OrderBy(x => x.AgeDate).ToList();
+                    break;
+                case "Hunger":
+                    pets = _pets.OrderBy(x => x.LastHungerLevel).Reverse().ToList();
+                    break;
+                case "Thirsty":
+                    pets = _pets.OrderBy(x => x.LastThirstyLevel).Reverse().ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            return View("InnogotchiList", pets);
+        }
+
+
+
     }
 }
